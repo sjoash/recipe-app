4af77687b8ab76d865737b0afbb7561a79bf0177
sjoash <joashjoachim@gmail.com>
2023-06-23 14:47:51 +0300

    changes

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,7 @@
+
+import * as model from './model.js';
+import recipeView from './recipeView.js';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +15,22 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function(){
+
+  try{
+    const id = window.location.hash.slice(1);
+    if(!id) return;
+    // Loading Recipe.
+    await model.loadRecipe(id);
+    
+    // Rendering Recipe.
+    recipeView.render(recipe.model.state);
+
+  }catch(err){
+    alert(err);
+  }
+};
+
+['hashchange','load'].forEach(ev => window.addEventListener(ev, controlRecipes));
+
